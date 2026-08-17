4e3f8d0bf04c172121197618d7b1686d7f1260f4
Nick Shahbaz <nickshahbaz135@gmail.com>
2026-08-17 00:18:08 -0400

    nothing crazy

---
+++ src/pages/Bench.tsx
@@ -0,0 +1,9 @@
+export default function Bench() {
+  return (
+    <div className="Bench">
+      <h1 className="text-3xl font-bold underline text-white h-screen flex items-center justify-center">
+        For Nick To Make
+      </h1>
+    </div>
+  )
+}
